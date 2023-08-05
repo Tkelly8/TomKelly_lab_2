dd81dbc13adcaf0093016d7e36c5bec7816c5b92
Tkelly8 <edenaun@gmail.com>
2023-08-05 19:58:01 +0000

    2nd commit asd
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,30 +2,31 @@
 
 // main() runs in its own thread in the OS
 //0,1,1,2,3,5,8,13........
+int fib(int n){
+    if(n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+    else
+        return(fib(n-1) + fib(n - 2));
+
+        
+
+}
+
 int main()
 {
-    printf("Lab 2 Program  \r\n");
-    int max_term = 10;
-    int term_1 = 0;
-    int term_2 = 1;
-    int next_term = 0;
+    printf("Lab 2 Program, \r\n");
+    printf("2nd commit , \r\n");
+    int max_term = 11;
+
     for(int i=1; i<=max_term; i++){
-        if(i == 1)
-            printf("%d, ", term_1);
-        if(i == 2)
-            printf("%d, ", term_2);
-        else{
-            ////3rd term = previous term
-            next_term = term_1 + term_2;
-            term_1 = term_2;
-            term_2 = next_term;
-            printf("%d, ", next_term);
-
-        }
+        printf("%d, ", fib(i));  
     }
-
     printf("\r\n");
     while (true) {
     }
-
+    
+    
 }
+
